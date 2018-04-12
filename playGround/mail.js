d1140827d7a75d6b7c7d2ae192859ab8130b13af
fyfyzzk <fyfyzzk@hotmail.com>
2018-04-11 23:59:19 -0700

    add playGround

---
+++ playGround/mail.js
@@ -0,0 +1,2 @@
+import func1, { color, name, Rectangle } from "utilities";
+console.log(fun1(9, 1));
---
+++ playGround/utilities.js
@@ -0,0 +1,25 @@
+// export data
+
+
+
+export var color = "red";
+export let name = "Mike";
+export const magicNumber = 7;
+
+// export function
+export function sum(num1, num2) {
+    return num1 + num1;
+}
+
+// export class
+export class Rectangle {
+    constructor(length, width) {
+        this.length = length;
+        this.width = width;
+    }
+}
+
+// export function as default
+export function subtract(num1, num2) {
+    return num1 - num1;
+}
