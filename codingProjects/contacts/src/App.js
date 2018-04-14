72c4b88743f6f9de7cb28a23c481266094cb0f40
fyfyzzk <fyfyzzk@hotmail.com>
2018-04-14 14:37:58 -0700

    add ListContacts component, display the basic infor including avatar, name, email

--- codingProjects/contacts/src/App.js
+++ codingProjects/contacts/src/App.js
@@ -1,33 +1,32 @@
 import React, { Component } from "react";
-class ContactList extends Component {
-    render() {
-        const people = this.props.contacts;
-        return (
-            <ol>
-                {people.map(person => <li key={person.name}>{person.name}</li>)}
-            </ol>
-        );
+import ListContacts from "./ListContacts";
+
+const contacts = [
+    {
+        id: "ryan",
+        name: "Ryan Florence",
+        email: "ryan@reacttraining.com",
+        avatarURL: "http://localhost:5001/ryan.jpg"
+    },
+    {
+        id: "michael",
+        name: "Michael Jackson",
+        email: "michael@reacttraining.com",
+        avatarURL: "http://localhost:5001/michael.jpg"
+    },
+    {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        email: "tyler@reacttraining.com",
+        avatarURL: "http://localhost:5001/tyler.jpg"
     }
-}
+];
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <ContactList
-                    contacts={[
-                        { name: "Michael" },
-                        { name: "Ryan" },
-                        { name: "Tyler" }
-                    ]}
-                />
-                <ContactList
-                    contacts={[
-                        { name: "Amanda" },
-                        { name: "Richard" },
-                        { name: "Geoff" }
-                    ]}
-                />
+            <div>
+                <ListContacts contacts={contacts} />
             </div>
         );
     }
