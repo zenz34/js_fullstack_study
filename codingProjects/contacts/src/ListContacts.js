b4bf3dbf056e96281a628e28e83820c14937b73f
fyfyzzk <fyfyzzk@hotmail.com>
2018-04-14 17:07:56 -0700

    replace svg, convert ListContacts to function component, add PropType npm module and add proptype check in the app.js and ListContacts.js

--- codingProjects/contacts/src/ListContacts.js
+++ codingProjects/contacts/src/ListContacts.js
@@ -1,27 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class ListContacts extends Component {
-    render() {
-        console.log("Props", this.props);
-        return (
-            <ol className="contact-list">
-                {this.props.contacts.map(contact => (
-                    <li key={contact.id} className="contact-list-item">
-                        <div
-                            className="contact-avatar"
-                            style={{
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}
-                        />
-                        <div className="contact-details">
-                            <p>{contact.name}</p>
-                            <p>{contact.email}</p>
-                        </div>
-                    </li>
-                ))}
-            </ol>
-        );
-    }
+function ListContacts(props) {
+    return (
+        <ol className="contact-list">
+            {props.contacts.map(contact => (
+                <li key={contact.id} className="contact-list-item">
+                    <div
+                        className="contact-avatar"
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                    />
+                    <div className="contact-details">
+                        <p>{contact.name}</p>
+                        <p>{contact.email}</p>
+                    </div>
+                    <button
+                        onClick={() => this.props.onDeleteContact(contact)}
+                        className="contact-remove"
+                    >
+                        Remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    );
 }
+
+
+ListContacts.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+};
 
 export default ListContacts;
