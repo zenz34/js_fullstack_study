efd3a147c2852340dd96617c59a357f1ecbd9e03
fyfyzzk <fyfyzzk@hotmail.com>
2018-04-23 19:17:01 -0700

    complete the create new contact page by adding: 1 avatar upload, 2 name input, 3 email inputk 4 create button, 5 back button

--- codingProjects/contacts/src/CreateContact.js
+++ codingProjects/contacts/src/CreateContact.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
+import ImageInput from "./ImageInput";
 class CreateContact extends Component {
     render() {
-        console.log("createContact");
-        return <p>create new Contact</p>;
+        return (
+            <div>
+                <Link className="close-create-contact" to="/">
+                    Close
+                </Link>
+                <form className="create-contact-form">
+                    <ImageInput
+                        className="create-contact-avatar-input"
+                        name="avatarURL"
+                        maxHeight={64}
+                    />
+                    <div className="create-contact-details">
+                        <input type="text" name="name" placeholder="Name" />
+                        <input type="text" name="email" placeholder="Email" />
+                        <button>Add Contact</button>
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
